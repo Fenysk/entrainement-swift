e7eb180e5d55792c893b858baae29a85f0eb7d50
Fenysk <minefenysk@gmail.com>
2023-10-16 10:11:42 +0200

    Initial Commit

---
+++ jv-stock-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jv-stock-ios
+//
+//  Created by Alexis Gailleton on 16/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jv-stock-ios/jv_stock_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  jv_stock_iosApp.swift
+//  jv-stock-ios
+//
+//  Created by Alexis Gailleton on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct jv_stock_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
